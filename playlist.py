ef101914a92ca641b97422314baf62885ad07ced
morganwashington-pixel <morgan.washington@ascte.org>
2026-09-22 08:42:57 -0500

    added song

--- playlist.py
+++ playlist.py
@@ -1,7 +1,7 @@
 SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
-    ("Viva La Vida", "Coldplay"),
+    ("Viva La Vida", "Coldplay"),("Sky full of Stars", "Coldplay")
 ]
 
 def print_playlist(songs):
